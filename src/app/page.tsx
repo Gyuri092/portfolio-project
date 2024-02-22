6e016fbd2d5af2bbc8bcdd12a65cb7062e77238c
Gyuri092 <ggr0927@gmail.com>
2024-02-22 16:37:49 +0900

    feat: Recoil 설치 및 atoms, selectors 생성

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,15 @@
 'use client';
 
 import { Header } from '@/components/Header/Header.client';
+import { RecoilRoot } from 'recoil';
 
 export default function Page() {
   return (
-    <div>
-      <Header />
-      <h1>Hello, Home page!</h1>
-    </div>
+    <RecoilRoot>
+      <div>
+        <Header />
+        <h1>Hello, Home page!</h1>
+      </div>
+    </RecoilRoot>
   );
 }
