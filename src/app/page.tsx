48de259d16035c0ef32a4995160fa111359e775c
Gyuri092 <ggr0927@gmail.com>
2024-04-03 13:02:06 +0900

    feat: RecoilRootWrapper 적용

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,17 @@
 'use client';
 
 import { Header } from '@/components/Header/Header.client';
-import { RecoilRoot } from 'recoil';
+import { darkModeState } from '@/recoil/atoms';
+import { useRecoilValue } from 'recoil';
 
-export default function Page() {
+export const Page = () => {
+  const darkMode = useRecoilValue(darkModeState);
   return (
-    <RecoilRoot>
-      <div>
-        <Header />
-        <h1>Hello, Home page!</h1>
-      </div>
-    </RecoilRoot>
+    <div className={darkMode ? 'darkmode' : 'lightmode'}>
+      <Header />
+      <h1>Hello, Home page!</h1>
+    </div>
   );
-}
+};
+
+export default Page;
