5f0f22228a3bc960944d4994d1bdaf5d5d69b3b0
Gyuri092 <ggr0927@gmail.com>
2024-04-03 13:39:16 +0900

    style: 헤더 및 글로벌 css 스타일 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,16 @@
 'use client';
 
 import { Header } from '@/components/Header/Header.client';
+import { MainPage } from '@/components/MainPage/MainPage';
 import { darkModeState } from '@/recoil/atoms';
 import { useRecoilValue } from 'recoil';
 
 export const Page = () => {
   const darkMode = useRecoilValue(darkModeState);
   return (
-    <div className={darkMode ? 'darkmode' : 'lightmode'}>
+    <div className={`${darkMode ? 'darkmode' : 'lightmode'} rootpage`}>
       <Header />
-      <h1>Hello, Home page!</h1>
+      <MainPage />
     </div>
   );
 };
