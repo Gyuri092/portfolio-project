02dd8e237f02daf19ef48b161e7d460b2da9b26a
Gyuri092 <ggr0927@gmail.com>
2024-04-09 00:01:10 +0900

    feat: 글자 타이핑 효과 구현

--- src/components/MainPage/MainPage.tsx
+++ src/components/MainPage/MainPage.tsx
@@ -1,12 +1,39 @@
 import { darkModeState } from '@/recoil/atoms';
 import '@/styles/mainpage.scss';
+import { useEffect, useState } from 'react';
 import { useRecoilValue } from 'recoil';
 
 export const MainPage = () => {
   const darkMode = useRecoilValue(darkModeState);
+  const [word, setWord] = useState('');
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    const sentence = 'Gyuri Gwon Portfolio';
+    const typingInterval = setInterval(() => {
+      setWord((prev) => {
+        const result = prev + sentence[count];
+        const nextCount = count + 1;
+        setCount(nextCount);
+
+        if (count >= sentence.length) {
+          // setCount(0);
+          clearInterval(typingInterval);
+          return prev;
+        }
+
+        return result;
+      });
+    }, 200);
+
+    return () => {
+      clearInterval(typingInterval);
+    };
+  }, [count]);
+
   return (
     <div className={`${darkMode ? 'darkmode' : 'lightmode'} mainpage`}>
-      Gyuri Gwon Portfolio
+      <p className="mainpage-text"> {word}</p>
     </div>
   );
 };
