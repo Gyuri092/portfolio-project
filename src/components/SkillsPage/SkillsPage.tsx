5b13a441f382a0ccc95df92b24576c1fab26cb07
Gyuri092 <ggr0927@gmail.com>
2024-04-23 16:41:03 +0900

    feat: skill에 대한 상세 설명 json 파일 생성, 버튼 클릭하여 보이도록 구현

--- src/components/SkillsPage/SkillsPage.tsx
+++ src/components/SkillsPage/SkillsPage.tsx
@@ -1,29 +1,103 @@
+import contentsJson from '@/components/SkillsPage/data/contents.json';
 import '@/styles/skillspage.scss';
+import { useMemo, useState } from 'react';
 
 export const SkillsPage = () => {
-  const objects = {
+  const [clickedSkill, setClickedSkill] = useState('');
+  const skillsOject = {
     'Front-End':
       'HTML, CSS\nJavaScript, TypeScript\nReact, Next.js\nRedux, Recoil, Zustand\nReact-query, SWR\nTailwindCSS, Styled-Components, Emotion',
     Database: 'Oracle, MS-SQL',
     Tools: 'VScode, Github, Gitlab\nFigma, Figjam\nNotion, Discord, Slack',
   };
+
+  const exceptionSkills = [
+    'HTML, CSS',
+    'JavaScript, TypeScript',
+    'Oracle, MS-SQL',
+  ];
+
+  const whenTextArray = contentsJson.when;
+  const whyTextArray = contentsJson.why;
+
+  const whenText = useMemo(() => {
+    return whenTextArray.find((whenElem) => whenElem.skill === clickedSkill);
+  }, [clickedSkill, whenTextArray]);
+  const whyText = useMemo(() => {
+    return whyTextArray.find((whyElem) => whyElem.skill === clickedSkill);
+  }, [clickedSkill, whyTextArray]);
+
   return (
     <div className="skills-page-container">
       <div className="skills-page-title">Skills</div>
       <div className="skills-page-contents">
-        {Object.entries(objects).map(([key, value]) => {
+        {Object.entries(skillsOject).map(([key, value]) => {
           return (
             <div key={key}>
               <p className="subtitle">{key}</p>
               <ul>
-                {value.split('\n').map((text) => (
-                  <li className="contents" key={text}>
-                    {text.split(',').map((elem, index) => (
-                      <button key={elem} className={key} type="button">
-                        {`${elem}${index !== text.split(',').length - 1 ? ',' : ''}`}
-                      </button>
-                    ))}
-                    {/* <div className="contents-detail">dsad</div> */}
+                {value.split('\n').map((skillName, skillNameIndex) => (
+                  <li className="contents" key={skillName}>
+                    {skillName
+                      .split(', ')
+                      .map((eachSkillName, eachSkillNameIndex) => {
+                        if (exceptionSkills[skillNameIndex] === skillName) {
+                          return (
+                            <button
+                              key={eachSkillName}
+                              className={key}
+                              type="button"
+                              onClick={() =>
+                                key !== 'Tools' &&
+                                setClickedSkill(
+                                  exceptionSkills[skillNameIndex] ?? '',
+                                )
+                              }
+                            >
+                              {`${exceptionSkills[skillNameIndex]}${eachSkillNameIndex !== skillName.split(',').length - 1 ? ',' : ''}`}
+                            </button>
+                          );
+                        }
+                        return (
+                          <button
+                            key={eachSkillName}
+                            className={key}
+                            type="button"
+                            onClick={() =>
+                              key !== 'Tools' && setClickedSkill(eachSkillName)
+                            }
+                          >
+                            {`${eachSkillName}${eachSkillNameIndex !== skillName.split(',').length - 1 ? ',' : ''}`}
+                          </button>
+                        );
+                      })}
+                    {skillName.split(', ').map((eachSkillName) => {
+                      if (exceptionSkills[skillNameIndex] === skillName) {
+                        return (
+                          clickedSkill === skillName && (
+                            <div
+                              key={eachSkillName}
+                              className="contents-detail"
+                            >
+                              <p>When?</p>
+                              <p>{whenText?.content}</p>
+                              <p>Why?</p>
+                              <p>{whyText?.content}</p>
+                            </div>
+                          )
+                        );
+                      }
+                      return (
+                        clickedSkill === eachSkillName && (
+                          <div key={eachSkillName} className="contents-detail">
+                            <p>When?</p>
+                            <p>{whenText?.content}</p>
+                            <p>Why?</p>
+                            <p>{whyText?.content}</p>
+                          </div>
+                        )
+                      );
+                    })}
                   </li>
                 ))}
               </ul>
