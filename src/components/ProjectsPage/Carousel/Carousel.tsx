5467d3eb169d94bf7ffb8aeaf0fb5552e4f9089f
Gyuri092 <ggr0927@gmail.com>
2024-05-22 22:10:21 +0900

    feat: 캐러셀 기능 구현중, chevron 회색 아이콘 추가

--- src/components/ProjectsPage/Carousel/Carousel.tsx
+++ src/components/ProjectsPage/Carousel/Carousel.tsx
@@ -1,32 +1,69 @@
 import ChevronLeftIcon from '@/icons/chevronLeft.svg';
+import ChevronLeftGrayIcon from '@/icons/chevronLeftGray.svg';
 import ChevronRightIcon from '@/icons/chevronRight.svg';
+import ChevronRightGrayIcon from '@/icons/chevronRightGray.svg';
 import '@/styles/carousel.scss';
 import Image from 'next/image';
+import { useState } from 'react';
 import imageDataJson from './data/imageData.json';
+
+const backgroundColors = [
+  'mbtiDetectiveBg', // Example background colors
+  'whiteBg',
+  'billiGBg',
+  'whiteBg',
+];
 
 export const Carousel = () => {
   const imageData = imageDataJson.array;
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const nextSlide = () => {
+    setCurrentIndex((prevIndex: number) => (prevIndex + 1) % imageData.length);
+  };
+
+  const prevSlide = () => {
+    setCurrentIndex(
+      (prevIndex: number) =>
+        (prevIndex - 1 + imageData.length) % imageData.length,
+    );
+  };
 
   return (
-    <div className="carousel-container">
-      <button type="button">
-        <ChevronLeftIcon />
-      </button>
-      {imageData.map((elem) => {
-        return (
-          <div key={elem.title} className="carousel-image-container">
+    <div className={`carousel-container ${backgroundColors[currentIndex]}`}>
+      <div className="chevron-button-container">
+        <button type="button" className="chevron-button" onClick={prevSlide}>
+          {backgroundColors[currentIndex] === 'whiteBg' ? (
+            <ChevronLeftGrayIcon />
+          ) : (
+            <ChevronLeftIcon />
+          )}
+        </button>
+        <button type="button" className="chevron-button" onClick={nextSlide}>
+          {backgroundColors[currentIndex] === 'whiteBg' ? (
+            <ChevronRightGrayIcon />
+          ) : (
+            <ChevronRightIcon />
+          )}
+        </button>
+      </div>
+      <div
+        className="carousel-image-container"
+        style={{ transform: `translateX(-${currentIndex * 50}%)` }}
+      >
+        {imageData.map((elem, index) => {
+          return (
             <Image
+              className={`carousel-image ${index === currentIndex ? 'active' : ''}`}
+              key={elem.title}
               src={elem.src}
               alt={elem.title}
               width={elem.size.width}
               height={elem.size.height}
             />
-          </div>
-        );
-      })}
-      <button type="button">
-        <ChevronRightIcon />
-      </button>
+          );
+        })}
+      </div>
     </div>
   );
 };
