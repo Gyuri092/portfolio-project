f95961b1523b8cee81c55f47cb338be4e105b09b
Gyuri092 <ggr0927@gmail.com>
2024-06-04 18:26:50 +0900

    feat: 모달 내부 자동 캐러셀 구현중(1)

--- src/components/Modal/ModalCarousel/ModalCarousel.tsx
+++ src/components/Modal/ModalCarousel/ModalCarousel.tsx
@@ -1,9 +1,40 @@
+import { currentCarouselIndexState } from '@/recoil/atoms';
 import '@/styles/modalcarousel.scss';
+import Image from 'next/image';
+import { useMemo } from 'react';
+import { useRecoilValue } from 'recoil';
+import modalCarouselImageJson from './data/modalCarouselImage.json';
 
 export const ModalCarousel = () => {
+  const imageSrcArray = modalCarouselImageJson.images;
+  const currentCarouselIndex = useRecoilValue(currentCarouselIndexState);
+
+  const imageSrc = useMemo(() => {
+    return imageSrcArray[currentCarouselIndex] ?? '';
+  }, [currentCarouselIndex, imageSrcArray]);
+
   return (
     <div className="modal-carousel-container">
-      이곳에 mbti명탐정 이미지 5개를 순환 재생하도록 설정해주세요.
+      <div
+        className="modal-carousel-image-container"
+        style={{ transform: `translateX(-${1 * 100}%)` }}
+      >
+        <div
+          key={imageSrcArray[currentCarouselIndex]}
+          className="modal-carousel-image-box"
+        >
+          {imageSrc.split('\n').map((image) => (
+            <Image
+              key={image}
+              className="modal-carousel-image"
+              src={image}
+              alt={image}
+              width={412}
+              height={630}
+            />
+          ))}
+        </div>
+      </div>
     </div>
   );
 };
