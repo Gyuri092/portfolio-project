b47ee43e54eb6b2e90b2a4b4a4193e597eaa1b66
Gyuri092 <ggr0927@gmail.com>
2024-06-06 23:53:22 +0900

    feat: 모달 내부 자동 캐러셀 구현중(2) - 자동 캐러셀 구현됨, 무한 캐러셀 구현 예정

--- src/components/Modal/ModalCarousel/ModalCarousel.tsx
+++ src/components/Modal/ModalCarousel/ModalCarousel.tsx
@@ -1,39 +1,66 @@
 import { currentCarouselIndexState } from '@/recoil/atoms';
 import '@/styles/modalcarousel.scss';
 import Image from 'next/image';
-import { useMemo } from 'react';
+import { useCallback, useEffect, useMemo, useState } from 'react';
 import { useRecoilValue } from 'recoil';
 import modalCarouselImageJson from './data/modalCarouselImage.json';
 
 export const ModalCarousel = () => {
-  const imageSrcArray = modalCarouselImageJson.images;
+  const imageJsonArray = modalCarouselImageJson.images;
   const currentCarouselIndex = useRecoilValue(currentCarouselIndexState);
+  const [currentIndex, setCurrentIndex] = useState(0);
 
   const imageSrc = useMemo(() => {
-    return imageSrcArray[currentCarouselIndex] ?? '';
-  }, [currentCarouselIndex, imageSrcArray]);
+    return imageJsonArray[currentCarouselIndex] ?? '';
+  }, [currentCarouselIndex, imageJsonArray]);
+
+  const imageSrcArray = useMemo(() => imageSrc.split('\n'), [imageSrc]);
+
+  const nextSlide = useCallback(() => {
+    if (currentIndex >= imageSrcArray.length + 1) {
+      setCurrentIndex(1);
+      return;
+    }
+    setCurrentIndex((prev) => (prev + 1) % imageSrcArray.length);
+  }, [currentIndex, imageSrcArray.length]);
+
+  useEffect(() => {
+    const timeout = setInterval(() => {
+      nextSlide();
+    }, 2000);
+    return () => clearInterval(timeout);
+  }, [nextSlide]);
 
   return (
     <div className="modal-carousel-container">
       <div
         className="modal-carousel-image-container"
-        style={{ transform: `translateX(-${1 * 100}%)` }}
+        style={{ transform: `translateX(-${currentIndex * 100}%)` }}
       >
-        <div
-          key={imageSrcArray[currentCarouselIndex]}
-          className="modal-carousel-image-box"
-        >
-          {imageSrc.split('\n').map((image) => (
-            <Image
-              key={image}
-              className="modal-carousel-image"
-              src={image}
-              alt={image}
-              width={412}
-              height={630}
-            />
-          ))}
-        </div>
+        <Image
+          className="modal-carousel-image"
+          src={imageSrcArray[imageSrcArray.length - 1] ?? ''}
+          alt={imageSrcArray[imageSrcArray.length - 1] ?? ''}
+          width={412}
+          height={630}
+        />
+        {imageSrcArray.map((image) => (
+          <Image
+            key={image}
+            className="modal-carousel-image"
+            src={image}
+            alt={image}
+            width={412}
+            height={630}
+          />
+        ))}
+        <Image
+          className="modal-carousel-image"
+          src={imageSrcArray[0] ?? ''}
+          alt={imageSrcArray[0] ?? ''}
+          width={412}
+          height={630}
+        />
       </div>
     </div>
   );
