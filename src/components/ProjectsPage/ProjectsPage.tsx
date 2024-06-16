c8a136c710d54793248a26c994d9f9de00d3dd4a
Gyuri092 <ggr0927@gmail.com>
2024-06-16 22:57:35 +0900

    fix: 다크모드 일괄 적용되도록 수정, object 변수 json파일로 분리 외 기타 정리 작업

--- src/components/ProjectsPage/ProjectsPage.tsx
+++ src/components/ProjectsPage/ProjectsPage.tsx
@@ -1,18 +1,30 @@
 import MediumIcon from '@/icons/medium.svg';
+import MediumIconInDarkMode from '@/icons/mediumInDarkMode.svg';
 import ViewCarouselIcon from '@/icons/viewCarousel.svg';
+import ViewCarouselIconInDarkMode from '@/icons/viewCarouselInDarkMode.svg';
+import { darkModeState } from '@/recoil/atoms';
 import '@/styles/projectspage.scss';
 import { useState } from 'react';
+import { useRecoilValue } from 'recoil';
 import { Carousel } from './Carousel/Carousel';
 import { MediumThumnailList } from './MediumThumnailList/MediumThumnailList';
 
 export const ProjectsPage = () => {
   const [viewCarousel, setViewCarousel] = useState(true);
+  const darkMode = useRecoilValue(darkModeState);
+
+  const mediumIcon = darkMode ? <MediumIconInDarkMode /> : <MediumIcon />;
+  const viewCarouselIcon = darkMode ? (
+    <ViewCarouselIconInDarkMode />
+  ) : (
+    <ViewCarouselIcon />
+  );
   return (
     <div className="projects-page-container">
       <p className="projects-page-title">Projects</p>
       <div className="projects-page-button-container">
         <button type="button" onClick={() => setViewCarousel((prev) => !prev)}>
-          {viewCarousel ? <MediumIcon /> : <ViewCarouselIcon />}
+          {viewCarousel ? mediumIcon : viewCarouselIcon}
         </button>
       </div>
       {viewCarousel ? <Carousel /> : <MediumThumnailList />}
