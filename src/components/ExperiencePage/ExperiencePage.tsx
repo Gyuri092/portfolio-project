d5e33030ec00634ee0e9d19d11b742b07f21c6ba
Gyuri092 <ggr0927@gmail.com>
2024-06-17 23:45:43 +0900

    fix: experience 페이지 레이아웃 수정

--- src/components/ExperiencePage/ExperiencePage.tsx
+++ src/components/ExperiencePage/ExperiencePage.tsx
@@ -7,35 +7,39 @@ export const ExperiencePage = () => {
 
   return (
     <div className="experience-page-container">
-      {contentsArray.map((object) => {
-        return (
-          <div key={object.title} className="experience-page-box">
-            <p className="experience-page-title">Experience</p>
-            <p className="experience-page-subtitle">{object.title}</p>
-            <div className="experience-page-contents">
-              <Image
-                src={object.src}
-                alt={`${object.title} screenshot`}
-                width={object.size.width}
-                height={object.size.height}
-              />
-              <div className="experience-page-text">
-                {object.link.map((elem) => (
-                  <a
-                    key={elem.name}
-                    href={elem.url}
-                    target="_blank"
-                    rel="noreferrer"
-                  >
-                    {elem.name}
-                  </a>
-                ))}
-                <p>{object.text}</p>
+      <p className="experience-page-title">Experience</p>
+      <div className="experience-page-middle-container">
+        {contentsArray.map((object) => {
+          return (
+            <div key={object.title} className="experience-page-box">
+              <div className="experience-page-contents">
+                <div className="experience-page-image-box">
+                  <p className="experience-page-subtitle">{object.title}</p>
+                  <Image
+                    src={object.src}
+                    alt={`${object.title} screenshot`}
+                    width={object.size.width}
+                    height={object.size.height}
+                  />
+                </div>
+                <div className="experience-page-text">
+                  {object.link.map((elem) => (
+                    <a
+                      key={elem.name}
+                      href={elem.url}
+                      target="_blank"
+                      rel="noreferrer"
+                    >
+                      {elem.name}
+                    </a>
+                  ))}
+                  <p>{object.text}</p>
+                </div>
               </div>
             </div>
-          </div>
-        );
-      })}
+          );
+        })}
+      </div>
     </div>
   );
 };
