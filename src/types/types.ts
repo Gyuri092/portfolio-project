08e2c492588e5fb1af7dedcc44c0e8a77c2ecd26
Gyuri092 <ggr0927@gmail.com>
2024-06-18 00:21:32 +0900

    fix: experience 페이지 수동 캐러셀로 구조 변경

--- src/types/types.ts
+++ src/types/types.ts
@@ -6,3 +6,24 @@ export type SkillListObjectType = {
   Database: string;
   Deployment: string;
 };
+
+type ImagePropertyType = {
+  [index: string]: number;
+  width: number;
+  height: number;
+};
+
+type LinkType = {
+  [index: string]: string;
+  name: string;
+  url: string;
+};
+
+export type ExperienceObjectType = {
+  [index: string]: string | ImagePropertyType | LinkType[];
+  title: string;
+  src: string;
+  size: ImagePropertyType;
+  link: LinkType[];
+  text: string;
+};
