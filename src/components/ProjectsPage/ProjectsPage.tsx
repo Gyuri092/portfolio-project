d8fc48209ddd1e9a13c86302b6cdd3add1e5eb4c
Gyuri092 <ggr0927@gmail.com>
2024-07-03 19:44:05 +0900

    fix: 펼쳐보기 아이콘 클릭 후 자동 슬라이드 동작 안하는 이슈 해결

--- src/components/ProjectsPage/ProjectsPage.tsx
+++ src/components/ProjectsPage/ProjectsPage.tsx
@@ -2,16 +2,17 @@ import MediumIcon from '@/icons/medium.svg';
 import MediumIconInDarkMode from '@/icons/mediumInDarkMode.svg';
 import ViewCarouselIcon from '@/icons/viewCarousel.svg';
 import ViewCarouselIconInDarkMode from '@/icons/viewCarouselInDarkMode.svg';
-import { darkModeState } from '@/recoil/atoms';
+import { darkModeState, stopIntervalState } from '@/recoil/atoms';
 import '@/styles/projectspage.scss';
 import { useState } from 'react';
-import { useRecoilValue } from 'recoil';
+import { useRecoilState, useRecoilValue } from 'recoil';
 import { Carousel } from './Carousel/Carousel';
 import { MediumThumnailList } from './MediumThumnailList/MediumThumnailList';
 
 export const ProjectsPage = () => {
   const [viewCarousel, setViewCarousel] = useState(true);
   const darkMode = useRecoilValue(darkModeState);
+  const [, setStopInterval] = useRecoilState(stopIntervalState);
 
   const mediumIcon = darkMode ? <MediumIconInDarkMode /> : <MediumIcon />;
   const viewCarouselIcon = darkMode ? (
@@ -23,7 +24,15 @@ export const ProjectsPage = () => {
     <div className="projects-page-container">
       <p className="projects-page-title">Projects</p>
       <div className="projects-page-button-container">
-        <button type="button" onClick={() => setViewCarousel((prev) => !prev)}>
+        <button
+          type="button"
+          onClick={() => {
+            setViewCarousel((prev) => !prev);
+            if (!viewCarousel) {
+              setStopInterval(false);
+            }
+          }}
+        >
           {viewCarousel ? mediumIcon : viewCarouselIcon}
         </button>
       </div>
