ec75f25e0171619c888256cfd6b73646975776cd
Gyuri092 <ggr0927@gmail.com>
2024-07-10 18:07:13 +0900

    chore: favicon 추가, metadata 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,11 @@
 import '@/styles/global.scss';
+import type { Metadata } from 'next';
 import RecoilRootWrapper from './RecoilRootWrapper';
-// import type { Metadata } from 'next';
 
-// export const metadata: Metadata = {
-//   title: 'Gyuri-Portpolio',
-//   description: 'This is Gyuri’s portfolio website.',
-// };
+export const metadata: Metadata = {
+  title: 'Gyuri-Portpolio',
+  description: 'This is Gyuri’s portfolio website.',
+};
 
 export default function RootLayout({
   children,
