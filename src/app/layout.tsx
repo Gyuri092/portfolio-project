ba5b4eb38c1c7b2cf1c1de1bd5a956c6605e71a5
Gyuri092 <ggr0927@gmail.com>
2024-07-11 00:41:37 +0900

    test: og image 생성 테스트 (2)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,14 @@ import RecoilRootWrapper from './RecoilRootWrapper';
 export const metadata: Metadata = {
   title: 'Gyuri-Portpolio',
   description: 'This is Gyuri’s portfolio website.',
+  openGraph: {
+    title: 'Gyuri-Portpolio',
+    description: 'This is Gyuri’s portfolio website.',
+    images: '/og-image.png',
+    siteName: 'Gyuri-Portpolio',
+    type: 'website',
+    locale: 'ko_KR',
+  },
 };
 
 export default function RootLayout({
