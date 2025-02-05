9cb91463a8c4ebffe0c1a4d0e4d8c1c4908d6851
Gyuri092 <ggr0927@gmail.com>
2025-02-05 21:48:17 +0900

    refactor: 전체 폰트 및 아이콘 사이즈 축소, profile, skills 간격 일관성 유지

--- src/components/SkillsPage/SkillsPage.tsx
+++ src/components/SkillsPage/SkillsPage.tsx
@@ -3,7 +3,7 @@ import SkillNameList from './SkillNameList/SkillNameList';
 
 const skillsOject = {
   'Front-End':
-    'HTML, CSS\nJavaScript, TypeScript\nReact, Next.js\nRedux, Recoil, Zustand\nReact-query, SWR\nTailwindCSS, Styled-Components, Emotion',
+    'HTML, CSS\nJavaScript, TypeScript\nReact, Next.js\nRedux, Recoil, Zustand\nReact-query, SWR\nTailwindCSS, Styled-Components\nEmotion, SaSS',
   Database: 'Oracle, MS-SQL',
   Tools: 'VScode, Github, Gitlab\nFigma, Figjam\nNotion, Discord, Slack',
 };
@@ -12,7 +12,7 @@ export default function SkillsPage() {
   return (
     <div className="page-container">
       <div className="page-title">Skills</div>
-      <div className="skills-page-contents">
+      <div className="page-contents">
         {Object.entries(skillsOject).map(([key, value]) => {
           return (
             <div key={key} className="skills-list-box">
