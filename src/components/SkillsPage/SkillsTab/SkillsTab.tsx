5c103fd7d33a1bef0616633b15d41d59f8ed7b97
Gyuri092 <ggr0927@gmail.com>
2025-02-18 22:15:49 +0900

    refactor: skills - 컴포넌트 분리완료

--- src/components/SkillsPage/SkillsTab/SkillsTab.tsx
+++ src/components/SkillsPage/SkillsTab/SkillsTab.tsx
@@ -1,17 +1,13 @@
-import FlipIcon from '@/icons/flip_royalPurple.svg';
-import DarkmodeFlipIcon from '@/icons/flip_white.svg';
-import { darkModeState } from '@/recoil/atoms';
-import { SkillsKeyType, SkillsType } from '@/types/types';
+import { SkillsType } from '@/types/types';
 import { useState } from 'react';
-import { useRecoilValue } from 'recoil';
-import skillsDetail from '../data/contents.json';
 import skills from '../data/skills.json';
+import { SkillsCardTable } from '../SkillsCardTable/SkillsCardTable';
+import { SkillsTabName } from '../SkillsTabName/SkillsTabName';
 
 export const SkillsTab = () => {
   const skillList: SkillsType = skills;
   const [currentTab, setCurrentTab] = useState<keyof SkillsType>('Front-End');
   const [flippedCards, setFlippedCards] = useState<string[]>([]);
-  const darkMode = useRecoilValue(darkModeState);
 
   const toggleCard = (item: string) => {
     setFlippedCards((prev) =>
@@ -21,70 +17,27 @@ export const SkillsTab = () => {
     );
   };
 
+  const updateCurrentTab = (key: keyof SkillsType) => setCurrentTab(key);
+  const updateFlippedCards = () =>
+    setFlippedCards((prev) =>
+      prev.length === skillList[currentTab].split('\n').length
+        ? []
+        : skillList[currentTab].split('\n'),
+    );
   return (
     <div>
-      <div className="skliis-page-tab">
-        <div>
-          {(Object.keys(skillList) as SkillsKeyType[]).map((key) => (
-            <button
-              type="button"
-              key={key}
-              className={`skills-page-tab-name ${key === currentTab && 'currentTab'}`}
-              onClick={() => setCurrentTab(key)}
-            >
-              {key}
-            </button>
-          ))}
-        </div>
-        <div className="skills-page-tab-divider">
-          <button
-            type="button"
-            className="skills-page-all-flip-button clickable"
-            onClick={() =>
-              currentTab !== 'Tools' &&
-              skillList[currentTab] &&
-              setFlippedCards((prev) =>
-                prev.length === skillList[currentTab].split('\n').length
-                  ? []
-                  : skillList[currentTab].split('\n'),
-              )
-            }
-          >
-            {darkMode ? <DarkmodeFlipIcon /> : <FlipIcon />}
-          </button>
-        </div>
-      </div>
-      <div className="skills-page-card-grid">
-        {skillList[currentTab] &&
-          skillList[currentTab].split('\n').map((item) => (
-            <div
-              key={item}
-              className={`skills-page-card-container ${flippedCards.includes(item) ? 'flipped' : ''}`}
-            >
-              <div className="skills-page-card-inner">
-                <button
-                  type="button"
-                  className={`skills-page-card front ${currentTab !== 'Tools' && 'clickable'}`}
-                  onClick={() => currentTab !== 'Tools' && toggleCard(item)}
-                >
-                  {item}
-                </button>
-                <button
-                  type="button"
-                  className={`skills-page-card back ${currentTab !== 'Tools' && 'clickable'}`}
-                  onClick={() => currentTab !== 'Tools' && toggleCard(item)}
-                >
-                  {`When:\n${
-                    skillsDetail.when.find((when) => when.skill === item)
-                      ?.content
-                  }\nWhy:\n${
-                    skillsDetail.why.find((why) => why.skill === item)?.content
-                  }`}
-                </button>
-              </div>
-            </div>
-          ))}
-      </div>
+      <SkillsTabName
+        skillList={skillList}
+        currentTab={currentTab}
+        updateCurrentTab={updateCurrentTab}
+        updateFlippedCards={updateFlippedCards}
+      />
+      <SkillsCardTable
+        skillList={skillList}
+        currentTab={currentTab}
+        flippedCards={flippedCards}
+        toggleCard={toggleCard}
+      />
     </div>
   );
 };
