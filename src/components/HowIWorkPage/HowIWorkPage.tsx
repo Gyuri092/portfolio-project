d4429646a5cc4318551f33ea646167e18baff657
Gyuri092 <ggr0927@gmail.com>
2025-02-19 21:05:26 +0900

    refactor: 반응형 css (mobile) 개편 완료

--- src/components/HowIWorkPage/HowIWorkPage.tsx
+++ src/components/HowIWorkPage/HowIWorkPage.tsx
@@ -3,13 +3,15 @@ import '@/styles/howiworkpage.scss';
 export default function HowIWorkPage() {
   return (
     <div className="howiworkpage-container">
-      <p className="howiworkpage-title">How I Work</p>
-      <p className="howiworkpage-contents">
-        {`문제에 맞닥뜨리면 명확하게 정의하고 해결하는 것을 좋아합니다.
+      <div className="howiworkpage-wrapper">
+        <p className="howiworkpage-title">How I Work</p>
+        <p className="howiworkpage-contents">
+          {`문제에 맞닥뜨리면 명확하게 정의하고 해결하는 것을 좋아합니다.
         어려운 문제를 해결하는 과정에서 재미를 느낍니다.
         새로운 지식이나 정보를 습득하는 일을 즐깁니다.
         동료 개발자와 문제를 같이 고민하고 해결하는 과정을 좋아합니다.`}
-      </p>
+        </p>
+      </div>
     </div>
   );
 }
--- src/components/SkillsPage/SkillsPage.tsx
+++ src/components/SkillsPage/SkillsPage.tsx
@@ -3,7 +3,7 @@ import { SkillsTab } from './SkillsTab/SkillsTab';
 
 export default function SkillsPage() {
   return (
-    <div className="page-container">
+    <div className="skills-page-container">
       <div className="page-title">Skills</div>
       <div className="skills-page-contents">
         <SkillsTab />
